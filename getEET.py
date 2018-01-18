6d3bf0e79ea79ff82f666bb9a33456b139c5bcfe
emmdim <savemanos@gmail.com>
2018-01-18 22:06:21 +0200

    Adds robustnes and retries to the url request

--- getEET.py
+++ getEET.py
@@ -1,13 +1,26 @@
 import urllib2
 import json
+import random
+import time
 
 TIMEOUT = 2
 TZDB_KEY = '459K79ZDCGC9'
+# 10 is chosen semi-arbitrarily, depending on the total number of nodes
+RETRIES = 10
 URL = 'http://api.timezonedb.com/v2/get-time-zone?format=json&format=json&by=zone&by=zone&zone=America/Chicago&zone=Europe/Madrid&key=%s' % TZDB_KEY
 
 def getEET():
-	req = urllib2.Request(URL)
-	opener = urllib2.build_opener()
-	f = opener.open(req, timeout=TIMEOUT)
-	result = json.loads(f.read())
-	return result['timestamp']
+	random.seed()
+	for n in range(0,RETRIES):
+		req = urllib2.Request(URL)
+		opener = urllib2.build_opener()
+		try:
+			f = opener.open(req, timeout=TIMEOUT)
+		except Exception:
+			print "Time request failed, attempting new one.."
+			# Create randomness for the concurrent requests when bootstrapping
+			time.sleep(random.random())
+		else:
+			result = json.loads(f.read())
+			print result['timestamp']
+			return result['timestamp']
