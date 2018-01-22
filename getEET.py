141b642e1e885f30643ae71875ba6a8f2dc9aea0
emmdim <savemanos@gmail.com>
2018-01-22 18:39:02 +0200

    Fixes compatibility with Python 2.4 where timeout arguments does not exist for urllib2 calls.

--- getEET.py
+++ getEET.py
@@ -3,19 +3,21 @@ import json
 import random
 import time
 
-TIMEOUT = 2
+TIMEOUT = 10
 TZDB_KEY = '459K79ZDCGC9'
 # 10 is chosen semi-arbitrarily, depending on the total number of nodes
 RETRIES = 10
 URL = 'http://api.timezonedb.com/v2/get-time-zone?format=json&format=json&by=zone&by=zone&zone=America/Chicago&zone=Europe/Madrid&key=%s' % TZDB_KEY
 
 def getEET():
+	import socket
+	socket.setdefaulttimeout(TIMEOUT)
 	random.seed()
 	for n in range(0,RETRIES):
 		req = urllib2.Request(URL)
 		opener = urllib2.build_opener()
 		try:
-			f = opener.open(req, timeout=TIMEOUT)
+			f = opener.open(req)
 		except Exception:
 			print "Time request failed, attempting new one.."
 			# Create randomness for the concurrent requests when bootstrapping
