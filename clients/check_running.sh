8dbd27e2c2ab32032939b112b105e96797a6b290
emmdim <savemanos@gmail.com>
2018-01-24 13:05:15 +0200

    * Implements a first working version of the check_running scripts * Step towards issue #1

--- clients/check_running.sh
+++ clients/check_running.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 # check_running.sh
 # make sure experiment is running.  
+pgrep -l -f 'requests.py' > /dev/null
+REQUESTS=$?
 
-if  ! pgrep -l -f 'requests.py' > /dev/null
-then
-	echo "Stopped"
+screen -ls | grep TTFB > /dev/null
+SCREEN=$?
+
+if [[ $REQUESTS -eq 0 && $SCREEN -eq 0 ]]; then
+	echo "Running"
+elif [[ $SCREEN -ne 0 ]]; then
+	echo "Screen is not running"
+	echo screen -dmS TTFB python requests.py restart && sleep 1
+else
+	echo "Requests are not running"
+	screen -S TTFB -X stuff "python requests.py restart"$(echo -ne '\015')
 fi
--- servers/check_running.sh
+++ servers/check_running.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 # check_running.sh
 # make sure experiment is running.  
+IFACE=$1
 
-if  pgrep -l -f 'monitor.py' > /dev/null
-then
+pgrep -l -f 'monitor.py' > /dev/null
+MONITOR=$?
+
+screen -ls | grep TTFB > /dev/null
+SCREEN=$?
+
+if [[ $MONITOR -eq 0 && $SCREEN -eq 0 ]]; then
 	echo "Running"
+elif [[ $SCREEN -ne 0 ]]; then
+	echo "Screen is not running"
+	echo screen -dmS TTFB python monitor.py $IFACE restart && sleep 1
 else
-	echo "Stopped"
+	echo "Monitor is not running"
+	screen -S TTFB -X stuff "python monitor.py $IFACE restart"$(echo -ne '\015')
 fi
