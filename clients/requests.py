45e6b334a989b19ce061794c2a924f637777b244
manos <manos@dync-42-252.ac.upc.edu>
2018-03-16 16:07:44 +0100

    Removes proxies that don't respond to pings

--- clients/requests.py
+++ clients/requests.py
@@ -38,9 +38,7 @@ PROXIES = {
     9 : '10.138.71.3',#8258-elpipa Centelles
     10 : '10.34.100.145', #Girona 17732-ProxySGElectronics1
     11 : '10.155.1.101',# 47181-BisbProxy Baix Emporda
-    12 : '10.138.73.3', #	8257-SFLLProxy Baix Llobregat
-    13 : '10.139.26.66', # 28170-Ripoll-SrverProxyEle
-    14 : '10.155.7.3', # 	23164-PALF-ProxyTorretes50 Palafrugell
+    12 : '10.155.7.3', # 	23164-PALF-ProxyTorretes50 Palafrugell
     #15 :
 
 }
